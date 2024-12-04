7adb89ee5d7a580bd68ca000c020c0931540eee0
mahdi <mahdiamr83@gmail.com>
2024-12-04 21:16:35 +0330

    add change method and ui class

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
 const weather = new Weather("تهران", "تهران");
-weather.changeLocation('فارس','شیراز')
-document.addEventListener('DOMContentLoaded',getWeather)
+
+const ui = new UI();
+
+document.addEventListener("DOMContentLoaded", getWeather);
+
+document
+  .getElementById("w-change-btn")
+  .addEventListener("click", changeLocation);
+
+async function changeLocation() {
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+
+  weather.changeLocation(state, city);
+
+  getWeather();
+
+  var myModalEl = document.getElementById("locationModal");
+  var modal = bootstrap.Modal.getInstance(myModalEl);
+  modal.hide();
+}
 
 function getWeather() {
   weather
     .getWeather()
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+    .then((result) => {
+      console.log(result);
+      ui.paint(result, weather.location);
+    })
+    .catch((err) => console.log(err.message));
 }
--- js/weather.js
+++ js/weather.js
@@ -9,16 +9,19 @@ class Weather {
     let response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apikey}`
     );
-    if (response.ok){
-        const data = await response.json();
-        return data
-    }else{
-        throw Error(response.status)
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw Error(response.status);
     }
   }
 
   changeLocation(state, city) {
     this.state = state;
     this.city = city;
+  }
+  get location() {
+    return this.state + " , " + this.city;
   }
 }
